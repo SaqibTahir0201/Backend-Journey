8a5ecb4454ba85496fb7b4b685ad0041602562f5
saqibtahir0201 <sqbmaaz@gmail.com>
2025-10-22 01:21:51 +0500

    middleware and error handler

--- day 5/routes/post.js
+++ day 5/routes/post.js
@@ -7,7 +7,24 @@ let posts = [
   { id: 3, title: "Post Three" },
 ];
 
+// middleware (better way it to make another folder with name middleware)
+
+// const logger = (req, res, next) => {
+//   console.log(
+//     `${req.method} ${req.protocol}://${req.get("host")}${req.orignalUrl}`
+//   );
+//   next();
+// };
+
 // Get all posts (with optional limit)
+// router.get("/", logger, (req, res) => {
+//   const limit = parseInt(req.query.limit);
+//   if (!isNaN(limit) && limit > 0) {
+//     return res.json(posts.slice(0, limit));
+//   }
+//   res.json(posts);
+// });
+
 router.get("/", (req, res) => {
   const limit = parseInt(req.query.limit);
   if (!isNaN(limit) && limit > 0) {
@@ -17,12 +34,17 @@ router.get("/", (req, res) => {
 });
 
 // Get post by ID
-router.get("/:id", (req, res) => {
+router.get("/:id", (req, res, next) => {
   const id = parseInt(req.params.id);
   const post = posts.find((p) => p.id === id);
+
   if (!post) {
-    return res.status(404).json({ error: "Post not found" });
+    const error = new Error(`a post with the id of ${id} was not found`);
+    next(error);
   }
+  // if (!post) {
+  //   return res.status(404).json({ error: "Post not found" });
+  // }
   res.json(post);
 });
 
--- day 5/server.js
+++ day 5/server.js
@@ -1,18 +1,23 @@
-import express from "express"
-import path from "path"
-import post from "./routes/post.js"
+import express from "express";
+import path from "path";
+import post from "./routes/post.js";
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/error.js";
 const port = 3000;
 const app = express();
 
 // Middleware to parse JSON bodies
-app.use(express.json());    
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// middleware
+app.use(logger);
 
+// routes
+app.use("/api/posts", post);
 
-
-app.use("/api/posts", post)
+//error handler (after using routes)
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
-
