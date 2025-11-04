527f4de749d24cd35a57fa9b1ea66f9f9675c3f2
saqibtahir0201 <sqbmaaz@gmail.com>
2025-11-04 23:35:58 +0500

    add login

--- day 8/controllers/userController.js
+++ day 8/controllers/userController.js
@@ -14,18 +14,49 @@ const registerUser = async (req, res) => {
     // const saltRound = 10;
     // const hash_password = await bcrypt.hash(password, saltRound);
 
-    const userCreate = await User.create({ name, email, password ,phone });
+    const userCreate = await User.create({ name, email, password, phone });
     // console.log("Hash:", hash_password);
 
     res.status(201).json({
       message: "User registered successfully",
       userCreate,
       token: await userCreate.generateToken(),
-      userId:userCreate._id.toString()
+      userId: userCreate._id.toString(),
     });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
 
-module.exports = { registerUser };
+// login logic
+const login = async function (req, res) {
+  try {
+    const { email, password } = req.body;
+    // email valid or not
+    const userExist = await User.findOne({ email });
+
+    if (!userExist) {
+      return res.status(400).json({ messege: "invalid credentials" });
+    }
+
+    // password compare
+
+    // const isPasswordValid = await bcrypt.compare(password, userExist.password);
+    const user = await bcrypt.compare(password, userExist.password);
+
+    if (user) {
+      res.status(200).json({
+        message: "User login successful",
+        userCreate,
+        token: await userCreate.generateToken(),
+        userId: userCreate._id.toString(),
+      });
+    }else{
+      res.status(401).json({ messege: "invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).join("internal server error");
+  }
+};
+
+module.exports = { registerUser,login };
--- day 8/routes/userRoutes.js
+++ day 8/routes/userRoutes.js
@@ -1,9 +1,11 @@
 
 const express = require("express");
 const { registerUser } = require("../controllers/userController");
+const { login } = require("../controllers/userController");
 const router = express.Router();
 
 // POST /api/users/register
 router.post("/register", registerUser);
+router.post("/login", login);
 
 module.exports = router;
