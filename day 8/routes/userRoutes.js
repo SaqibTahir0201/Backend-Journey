5e5b86849a7a4dcfc0d15f67f9024ca69b8b6487
saqibtahir0201 <sqbmaaz@gmail.com>
2025-11-08 22:16:22 +0500

    update

--- day 8/routes/userRoutes.js
+++ day 8/routes/userRoutes.js
@@ -3,9 +3,13 @@ const express = require("express");
 const { registerUser } = require("../controllers/userController");
 const { login } = require("../controllers/userController");
 const router = express.Router();
+import UserController from "../controllers/userController.js"
+
 
 // POST /api/users/register
-router.post("/register", registerUser);
-router.post("/login", login);
+// router.post("/register", registerUser);
+// router.post("/login", login);
+router.post("/register", UserController.registerUser);
+router.post("/login", UserController.login);
 
 module.exports = router;
