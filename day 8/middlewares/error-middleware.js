949c05bb6cba03be40ddbef3d63c39f7d51b1117
saqibtahir0201 <sqbmaaz@gmail.com>
2025-11-15 01:04:46 +0500

    fix bug

--- day 8/middlewares/error-middleware.js
+++ day 8/middlewares/error-middleware.js
@@ -1,7 +1,7 @@
 const errorMiddleware = (err, req, res, next) => {
-  const status = err.status | 500;
-  const message = err.message | "error from backend";
-  const extraDetails = err.extraDetails | "error from backend";
+  const status = err.status || 500;
+  const message = err.message || "error from backend";
+  const extraDetails = err.extraDetails || "error from backend";
 
   return res.status(status).json(message, extraDetails);
 };
